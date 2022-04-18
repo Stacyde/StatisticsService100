ad836d874d3197f5a355c9d03eab27684de752ad
Stacyde <degtich@mail.ru>
2022-04-18 15:14:49 +0400

    added test findmin

--- src/test/java/ru/netology/StatisticsServiceTest.java
+++ src/test/java/ru/netology/StatisticsServiceTest.java
@@ -5,16 +5,18 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatisticsServiceTest {
+
     @Test
-    void findMax() {
+    void findMin() {
         StatisticsService service = new StatisticsService();
 
         long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
-        long expected = 12;
+        long expected = 3;
 
-        long actual = service.findMax(incomesInBillions);
+        long actual = service.findMin(incomesInBillions);
 
         assertEquals(expected, actual);
     }
-
 }
+
+
